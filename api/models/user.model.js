f2511c7921c42b121ac74c7d2979813982d9d7ed
Student ApnaCollege <abdulwadoodowner@gmail.com>
2024-02-11 15:32:49 +0500

    create and add OaUTH COMPONENT

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -5,6 +5,11 @@ const userSchema = new mongoose.Schema(
     username: { type: String, required: true, unique: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
+    avatar: {
+      type: String,
+      default:
+        "https://www.google.com/imgres?imgurl=https%3A%2F%2Fbeforeigosolutions.com%2Fwp-content%2Fuploads%2F2021%2F12%2Fdummy-profile-pic-300x300-1.png&tbnid=lwajYrMcLkqoMM&vet=12ahUKEwi304PGhKOEAxVaVqQEHTAlANIQMygHegUIARCAAQ..i&imgrefurl=https%3A%2F%2Fbeforeigosolutions.com%2Fpascale-atkinson%2Fattachment%2Fdummy-profile-pic-300x300-1%2F&docid=-be1M6COtQJYCM&w=300&h=300&q=img%20dumy&ved=2ahUKEwi304PGhKOEAxVaVqQEHTAlANIQMygHegUIARCAAQ",
+    },
   },
   { timestamps: true }
 );
---
+++ client/src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: "mern-estate-87c8a.firebaseapp.com",
+  projectId: "mern-estate-87c8a",
+  storageBucket: "mern-estate-87c8a.appspot.com",
+  messagingSenderId: "413299592916",
+  appId: "1:413299592916:web:d7c0a6b6c599596348f541",
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
