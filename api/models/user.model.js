9423a6c5d444f508043d128f7ee2bb06221db609
Student ApnaCollege <abdulwadoodowner@gmail.com>
2024-02-11 16:08:41 +0500

    update the header and make the profile page private

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -8,7 +8,7 @@ const userSchema = new mongoose.Schema(
     avatar: {
       type: String,
       default:
-        "https://www.google.com/imgres?imgurl=https%3A%2F%2Fbeforeigosolutions.com%2Fwp-content%2Fuploads%2F2021%2F12%2Fdummy-profile-pic-300x300-1.png&tbnid=lwajYrMcLkqoMM&vet=12ahUKEwi304PGhKOEAxVaVqQEHTAlANIQMygHegUIARCAAQ..i&imgrefurl=https%3A%2F%2Fbeforeigosolutions.com%2Fpascale-atkinson%2Fattachment%2Fdummy-profile-pic-300x300-1%2F&docid=-be1M6COtQJYCM&w=300&h=300&q=img%20dumy&ved=2ahUKEwi304PGhKOEAxVaVqQEHTAlANIQMygHegUIARCAAQ",
+        "https://beforeigosolutions.com/wp-content/uploads/2021/12/dummy-profile-pic-300x300-1.png",
     },
   },
   { timestamps: true }
