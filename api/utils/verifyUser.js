659f43ec97d4a5278f205b5fa51873b9104783b0
AbdulWadood2 <abdulwadoodowner@gmail.com>
2024-03-04 01:15:18 +0500

    ok

--- api/utils/verifyUser.js
+++ api/utils/verifyUser.js
@@ -1,8 +1,7 @@
 import { errorHandler } from "./error.js";
 import jwt from "jsonwebtoken";
 export const verifyToken = (req, res, next) => {
-  const token = req.cookies.accessToken;
-
+  const token = req.cookies.access_token;
   if (!token) return next(errorHandler(401, "Unauthorized"));
   jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
     if (err) return next(errorHandler(403, "Forbidden"));
