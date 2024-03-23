6a0aaf63500c1e8dca29d18f211d1f0bc25aec6c
AbdulWadood2 <abdulwadoodowner@gmail.com>
2024-03-23 23:40:47 +0500

    complete about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,7 +1,13 @@
-import React from 'react'
+import React from "react";
 
 export default function About() {
   return (
-    <div>About</div>
-  )
+    <div className="py-20">
+      <h1 className="text-3xl font-bold mb-4 text-slate-800">About</h1>
+      <p className="mb-4  text-slate-700">
+        Sahend Estate is the best place to find your next perfect place to live.
+        We have a wide range of properties for you to choose from.
+      </p>
+    </div>
+  );
 }
